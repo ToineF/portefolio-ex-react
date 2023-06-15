67f1428f34de6c8248630e3bc09d43664ead232b
ToineF <antoine.foucault777@gmail.com>
2023-06-15 09:36:33 +0200

    button prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./styles.css"
+import MenuButton from "./App2/MenuButton"
+
 export default function App() {
   return (
     <>
     <div className="form-row">
-    <a class="btn" href="/src/App2/App2.html">
-        <h5>EX 1 : Counter</h5>
-      </a>
-      <a class="btn" href="/src/App2/App2.html">
-        <h5>EX 2 : To Do List</h5>
-      </a>
+      <MenuButton name="EX 1 : Counter" link="/src/App2/App2.html"/>
+      <MenuButton name="EX 2 : To Do List" link="/src/App2/App2.html"/>
     </div>
     </>
   )
