f9b5a1769dda6f9bb25d6b4c9b15e116ee5901bf
ToineF <antoine.foucault777@gmail.com>
2023-06-16 13:31:00 +0200

    working celsisus fahrenheit conversion

--- src/App3/App3.jsx
+++ src/App3/App3.jsx
@@ -6,42 +6,31 @@ export default function App3() {
 
   const [sentence, setSentence] = useState("")
   const [celsiusInput, setCelsiusInput] = useState("")
-  const [fahrenheitInput, setfahrenheitInput] = useState("popo")
+  const [fahrenheitInput, setfahrenheitInput] = useState("")
 
   function formSubmit(event) {
     event.preventDefault();
-    console.log(typeof celsiusInput) // return undefined
     if (isNaN(celsiusInput)) return
     setSentence((celsiusInput > 100)?"C'est bouillant":"Ce n'est pas bouillant")
-  }
-
-  function changeCelsisus(e) {
-     setCelsiusInput(() => {
-       e.target.value // return string
-     })
-    // if (isNaN(celsiusInput)) return
-
-    //setfahrenheitInput(() => celsiusInput)
-    //"celsiusInput*1.8 + 32"
-  }
-
-  function changeFahrenheit(e) {
-    setfahrenheitInput(() => {
-      e.target.value
-    })
   }
 
   return (
     <div className="form-row">
       <form onSubmit={formSubmit} className="new-item-form">
+        <p>Celsisus</p>
         <input type="text" value={celsiusInput} onChange={(e) => {
-        setCelsiusInput(e.target.value)}}/>
+        setCelsiusInput(e.target.value)
+        setfahrenheitInput((isNaN(e.target.value) || e.target.value === "")?(e.target.value):(e.target.value*1.8 + 32))}}/>
         <button className="btn">Enter</button>
       </form>
-      {/* <form onSubmit={formSubmit} className="new-item-form">
-        <input type="text" value={fahrenheitInput} onChange={changeFahrenheit}/>
+      <form onSubmit={formSubmit} className="new-item-form">
+        <p>Fahrenheit</p>
+        <input type="text" value={fahrenheitInput} onChange={(e) => {
+        setCelsiusInput((isNaN(e.target.value) || e.target.value === "")?(e.target.value):((e.target.value- 32)/1.8 ))
+        setfahrenheitInput(e.target.value)}}/>
         <button className="btn">Enter</button>
-      </form> */}
+      </form>
+      
       <p>{sentence}</p>
       <MenuButton name="Menu" link ="../../index.html"/>
     </div>
