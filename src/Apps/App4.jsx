65e95530b7ecd64ccc9abb67b071f32aeb3ecabe
ToineF <antoine.foucault777@gmail.com>
2023-06-20 09:15:03 +0200

    is fetching message

--- src/Apps/App4.jsx
+++ src/Apps/App4.jsx
@@ -5,15 +5,21 @@ export default function App4() {
   const [books, setBooks] = useState([])
   const [isFetching, setIsFetching] = useState(true)
 
+
 useEffect(() => {
-    fetch("https://fakerapi.it/api/v1/books?_quantity=9")
-        .then(res => res.json())
-        .then(data => setBooks(data.data))
+    async function fetchAPI() {
+      setIsFetching(true)
+      const res = await fetch("https://fakerapi.it/api/v1/books?_quantity=9")
+      const data = await res.json()
+      setBooks(data.data)
+      setIsFetching(false)
+    }
+  fetchAPI()
 }, [])
 
   return (
     <div className="form-row">
-      {books.map(book => <div>{book.title}</div>)}
+      {(isFetching)?(<div>Fetching Data...</div>):(books.map(book => <div>{book.title}</div>))}
     </div>
   )
 }
