09a37695e54a81966cb646b59c2562fcfcf03c7d
ToineF <antoine.foucault777@gmail.com>
2023-06-20 14:57:03 +0200

    alphabetical order buttons

--- src/Apps/App4.jsx
+++ src/Apps/App4.jsx
@@ -19,15 +19,26 @@ useEffect(() => {
   fetchAPI()
 }, [])
 
+const changeOrder = (condition) => {
+  let newbooks = [...books]
+  newbooks.sort((a,b) => {
+    if(a.title < b.title) { return -1 * condition; }
+    if(a.title > b.title) { return 1 * condition; }
+    return 0;
+  })
+  setBooks(newbooks)
+  console.log(books)
+}
+
   return (
     <div className="form-row mw-auto">
       <h1 className="font-bold">Book List:</h1>
-      {/* <div className="flex wrapper flex-row gap-2">
-        <button className="btn">
+      <div className="flex wrapper flex-row gap-2">
+        <button className="btn" onClick={() => changeOrder(1)}>
           A-Z</button>
-        <button className="btn">
+        <button className="btn" onClick={() => changeOrder(-1)}>
           Z-A</button>
-      </div> */}
+      </div>
       <div className="grid cards-container gap-4">
         {(isFetching)?(<div>Fetching Data...</div>):(
           books.map(book => <BookCard book={book}></BookCard>
