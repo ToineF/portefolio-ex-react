3ed734badad30620b6401aefb489fb98c51e1155
ToineF <antoine.foucault777@gmail.com>
2023-06-22 14:34:21 +0200

    query devtools successful import

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,29 @@ import App5 from "./Apps/App5"
 import AppHome from "./Apps/AppHome"
 import AppError from "./Apps/AppError"
 import NavBar from "./Components/NavBar"
-import { QueryClient, QueryClientProvider } from "react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools"
+import { QueryClientProvider, QueryClient } from "react-query"
+import { ReactQueryDevtools } from 'react-query/devtools'
 
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
     <>
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<AppHome/>}/>
-        <Route element={<NavBar/>}>
-          <Route path="/ex1" element={<App1/>}/>
-          <Route path="/ex2" element={<App2/>}/>
-          <Route path="/ex3" element={<App3/>}/>
-        </Route>
-        <Route path="/ex4" element={<App4/>}/>
-        <Route path="/ex5" element={<App5/>}/>
-        <Route path="*" element={<AppError/>} />
-      </Routes>
-      <ReactQueryDevtools/>
-    </QueryClientProvider>
+        <Routes>
+          <Route path="/" element={<AppHome/>}/>
+          <Route element={<NavBar/>}>
+            <Route path="/ex1" element={<App1/>}/>
+            <Route path="/ex2" element={<App2/>}/>
+            <Route path="/ex3" element={<App3/>}/>
+          </Route>
+          <Route path="/ex4" element={<App4/>}/>
+          <Route path="/ex5" element={<App5/>}/>
+          <Route path="*" element={<AppError/>} />
+        </Routes>
+        <ReactQueryDevtools/>
+      </QueryClientProvider>
     </>
   )
 }
